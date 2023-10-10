5d4ec7e5a8e93380dac781b6276ba1015fc1c3f3
Prasanth NV <pnellikkaparambil@opentrends.net>
2023-10-10 14:37:47 +0530

    added tailwind and test app

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,19 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  let count: number = 0;
+  $: double = count * 2;
+  const onClick = () => {
+    count++;
+  };
 </script>
 
-<main>
+<div
+  class="w-[100vw] h-[100vh] flex align-middle bg-gray-200 flex-row justify-center items-center"
+>
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+    <h1 class="text-3xl font-bold italic text-gray-700">Hello world!</h1>
+    <button on:click={onClick} class="bg-red-400 p-8 active:bg-red-500"
+      >{count}</button
+    >
+    <label for="">Doubled Value : {double}</label>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+</div>
