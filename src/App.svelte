e9d2a2c28d95e3f55d8bd8a71fcde5de56dffbc3
Prasanth NV <pnellikkaparambil@opentrends.net>
2023-10-11 17:37:48 +0530

    added public home page

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,18 @@
 <!-- App.svelte -->
 <script>
-  import { Router, Link, Route } from "svelte-routing";
-  import Home from "./modules/user/pages/home.svelte";
-  import Poll from "./modules/user/pages/poll.svelte";
-  export let url = "";
+  import { Router, Link, Route } from 'svelte-routing';
+  import Home from './routes/public/home.svelte';
+  export let url = '';
 </script>
 
 <Router {url}>
-  <nav>
+  <!-- <nav class="absolute">
     <Link to="/">Home</Link>
-    <Link to="/poll">Poll</Link>
-  </nav>
+  </nav> -->
   <div>
     <!-- <Route path="/blog/:id" component={BlogPost} />
     <Route path="/blog" component={Blog} />
     <Route path="/about" component={About} /> -->
     <Route path="/"><Home /></Route>
-    <Route path="/poll"><Poll /></Route>
   </div>
 </Router>
--- src/modules/user/pages/poll.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-  let pagename = prompt("Enter page name");
-</script>
-
-<h1>Hello from {pagename}</h1>
