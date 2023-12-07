64e8d9529b28f2eaf22c9afde96b7514417b2612
haripriya <haripriyas@opentrends.net>
2023-12-07 16:07:46 +0530

    add the validation message for confirm password

--- src/schema/auth/register.schema.ts
+++ src/schema/auth/register.schema.ts
@@ -1,15 +1,12 @@
-import * as Yup from "yup";
+import * as Yup from 'yup';
 
 export const RegisterSchema = Yup.object().shape({
-  email: Yup.string()
-    .required("* Email field is required")
-    .email("* Please enter a valid email"),
-  name: Yup.string().required("* Name field is required"),
+  email: Yup.string().required('* Email field is required').email('* Please enter a valid email'),
+  name: Yup.string().required('* Name field is required'),
   password: Yup.string()
-    .min(8, "* Password should be at least 8 characters")
-    .required("* Password is required"),
-  confirmPassword: Yup.string().oneOf(
-    [Yup.ref("password"), ""],
-    "Passwords must match"
-  ),
+    .min(8, '* Password should be at least 8 characters')
+    .required('* Password is required'),
+  confirmPassword: Yup.string()
+    .required('* Password is required')
+    .oneOf([Yup.ref('password'), ''], 'Passwords must match')
 });
